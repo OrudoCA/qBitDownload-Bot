50fa098102398437362d0be0409558498a376745
trueold89 <trueold89@orudo.ru>
2023-11-01 02:07:35 +0300

    Add English translation

--- bot/lang.py
+++ bot/lang.py
@@ -3,7 +3,7 @@
 
 import os
 
-langs = ['RU']
+langs = ['RU','ENG']
 
 # Russian
 RU = {
@@ -28,10 +28,27 @@ RU = {
         }
 
 # English
-ENG = {}
+ENG = {
+        'alauth': 'You are already authorized',
+        'sucauth': 'You have successfully logged in',
+        'wrauth': 'Wrong password',
+        'pne': "The '{}' directory does not exist on the server",
+        'fsa': "The '{}' folder has been successfully added",
+        'frm': "The '{}' folder has been successfully deleted", 
+        'fne': "The '{}' folder does not exist",
+        'add': 'Torrent has been added to the queue',
+        'type': 'Select the download type:',
+        'magnet': 'Magnet',
+        'file': 'File',
+        'aerr': 'Wrong arguments',
+        'cff': 'No folders found, use the /add command',
+        'chf': 'Select folder:',
+        'ntorr': 'Incorrect file extension',
+        'sendm': 'Send Magnet link',
+        'sendf': 'Send .torrent file',
+        'adeny': "You do not have access, first authorize '/login <password>'"
+        }
 
 for i in langs:
-    if i == os.environ['LANG']:
+    if i == os.environ.get('LANG','RU'):
         LANG = globals()[i]
-    else:
-        LANG = RU
